6de7a609e229fd2e6e14f1cdbf08b1445a56e142
Maxence Liboz <mliboz@student.42lyon.fr>
2024-09-02 14:43:38 +0200

    [Evo] Main structure and datastructure + heurisitics

---
+++ Datastructure.hpp
@@ -0,0 +1,50 @@
+#ifndef DATASTRUCTURE_HPP
+#define DATASTRUCTURE_HPP
+
+#include <vector>
+#include <tgmath.h>
+#include <stdexcept>
+
+enum Heuristic
+{
+    MANHATTAN_DISTANCE,
+    MISPLACED_TILES,
+    LINEAR_CONFLICT_AND_MANHATTAN_DISTANCE
+};
+
+class Datastructure
+{
+private:
+    std::vector<int> puzzle;
+    double fxScore;
+    Datastructure *parent;
+    int dim;
+    Heuristic heuristic;
+    int gxScore;
+
+    bool isLinearConflict(int i, int j);
+    void setParentPuzzle(std::vector<int> puzzle);
+
+public:
+    // Constructors and Destructors
+    Datastructure(std::vector<int> puzzle, Heuristic heuristic, Datastructure *parent);
+    ~Datastructure();
+
+    // Getters
+    std::vector<int> getPuzzle();
+    double getFxScore();
+    Datastructure *getParent();
+
+    // Setters
+    void setFxScore();
+
+    // Heuristic functions
+    double calculateManhattanDistance();
+    double calculateMisplacedTiles();
+    double calculateLinearConflictAndManhattanDistance();
+
+    // Move functions
+    std::vector<Datastructure *> getChildren();
+};
+
+#endif
