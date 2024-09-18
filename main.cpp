258d6cbb33b444e5635c82a6b80b01eefce05816
Anas Zanane <azanane@z2r12p4.42lyon.fr>
2024-09-18 15:17:45 +0200

    [Evo] Update main with Maxence's comments

--- main.cpp
+++ main.cpp
@@ -7,14 +7,16 @@ int main(int ac, char **av)
     std::priority_queue<Node *, std::vector<Node*>, CmpNodePtr> openList;
     std::unordered_set<std::vector<int>, HashNode> closedList;
 
-	std::ifstream inputFile;
-    inputFile.open(av[1]);
-	if (inputFile.fail()) {
-		
-		throw std::invalid_argument("Invalid text file");
-	}
+    Parser parser = Parser(ac, av);
 
-    Parser parser = Parser(ac, av, inputFile);
+    try {
+
+        parser.parsing();
+
+    } catch (std::invalid_argument &e) {
+        std::cout << e.what() << std::endl;
+        return 1;
+    }
 
 	std::vector<int> vec = parser.getPuzzle();
 
