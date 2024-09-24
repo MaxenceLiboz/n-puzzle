a9f32cb996a953953bbd13dad37553f3eaa404c2
Anas Zanane <azanane@z1r11p2.42lyon.fr>
2024-09-24 14:14:04 +0200

    [Evo] Finished version of the random generator

--- RandomGenerator.cpp
+++ RandomGenerator.cpp
@@ -5,15 +5,37 @@ RandomGenerator::RandomGenerator(int dim) {
     std::vector<int>    puzzle;
 
     srand((unsigned) time(NULL));
+    int puzzleSize = pow(dim, 2);
 
-    for (int i = 0; i < pow(dim, 2); i++) {
+    for (int i = 0; i < puzzleSize; i++) {
 
-        int puzzleSize = pow(dim, 2);
-	    puzzle.push_back(rand() % puzzleSize);
+        int randomAdd = rand() % (puzzleSize - 1);
+
+        if (puzzle.size() > 0) {
+
+                for (std::vector<int>::iterator iter = puzzle.begin(); iter != puzzle.end(); ++iter) {
+
+                    if (randomAdd == *iter) {
+
+                        if (randomAdd == puzzleSize - 1)
+                            randomAdd = 0;
+                        else
+                            randomAdd++;
+                        
+                        // At the next iteration, iter will be equal to puzzle.begin()
+                        iter = puzzle.begin() - 1;
+                    }
+                }
+        }
+        
+        puzzle.push_back(randomAdd);   
     }
-
-    for (int i : puzzle)
-        std::cout << i << " ";
+    
+    _setPuzzle(puzzle);
 }
 
 RandomGenerator::~RandomGenerator() {}
+
+void RandomGenerator::_setPuzzle(std::vector<int> puzzle) { this->_puzzle = puzzle; }
+
+std::vector<int> RandomGenerator::getPuzzle() const { return this->_puzzle; }
