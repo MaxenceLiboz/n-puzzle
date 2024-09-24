94c2de1b5dab576a2412a78c8bff7d88526be182
Anas Zanane <azanane@z1r11p2.42lyon.fr>
2024-09-24 14:43:08 +0200

    [Test] Add a test main

--- main.cpp
+++ main.cpp
@@ -2,19 +2,20 @@
 #include <unordered_set>
 
 int main() {
-    std::vector<int> v = {1, 7, 2, 0, 8, 4, 3, 6, 5};
-    NPuzzle puzzle(v, MANHATTAN_DISTANCE);
-
-    // 1, 7, 2, 0, 8, 4, 3, 6, 5
-    // 0,7,2,1,8,4,3,6,5
-    // 0,6,2,8,7,3,5,4,1
-    // 4, 6, 5, 7, 3, 8, 1, 2, 0 Très long
-    // 4, 2, 3, 1, 7, 6, 8, 0, 5 Un enfant très rapide
-    // NPuzzle puzzle(3, MANHATTAN_DISTANCE);
 
     try {
+
+        RandomGenerator gen;
+        gen.generateRandom(3);
+        
+        for (int i : gen.getPuzzle())
+            std::cout << i << " ";
+
+        NPuzzle puzzle(gen.getPuzzle(), MANHATTAN_DISTANCE);
+
         puzzle.solve();
         return 0;
+
     } catch (std::invalid_argument &e) {
         std::cout << e.what() << std::endl;
         return 1;
